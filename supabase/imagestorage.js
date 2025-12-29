0fd0b0819a5d76cc3c88c292e2fcbfdb100defe2
Paras Gathani <gathanip@gmail.com>
2025-12-30 00:22:19 +0700

    fix supabase

--- supabase/imagestorage.js
+++ supabase/imagestorage.js
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+require("dotenv").config();
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
