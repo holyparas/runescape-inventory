bbb5d0dbadbbd89928870515ce0e6e5dd2830dcc
Paras Gathani <gathanip@gmail.com>
2025-12-29 22:04:03 +0700

    ssl remove

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 module.exports = new Pool({
   connectionString: process.env.DB_STRING,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
 });
